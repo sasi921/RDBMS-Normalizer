6b5e0aa2fafb50a3c0a1d4e5b60f647a1c10acbc
sasi921 <124865456+sasi921@users.noreply.github.com>
2024-11-11 15:46:41 -0600

    Update outputallformtables.py

--- outputallformtables.py
+++ outputallformtables.py
@@ -1,5 +1,4 @@
-import pandas as pd
-
+# output generating as per userschoice
 def querydatatypes(dtype):
     if "int" in str(dtype):
         return "INT"
@@ -12,34 +11,51 @@ def querydatatypes(dtype):
     else:
         return "TEXT"
 
-def op1NF(primary_keys, df):
-    primary_key = primary_keys[0] if len(primary_keys) == 1 else primary_keys
-    table_name = "_".join(primary_key) + "_table" if isinstance(primary_key, list) else primary_key + "_table"
+
+def op1NF(Primarykeys, df):
+    Primarykey = list(df.keys())[0]
+    table_name = "_".join(Primarykey) + "_table"
+    df = df[Primarykey]
     query = f"CREATE TABLE {table_name} (\n"
 
     for column, dtype in zip(df.columns, df.dtypes):
-        if column in primary_keys:
+        if column in Primarykeys:
             query += f"  {column} {querydatatypes(dtype)} PRIMARY KEY,\n"
         else:
             query += f"  {column} {querydatatypes(dtype)},\n"
 
     query = query.rstrip(',\n') + "\n);"
+
     print(query)
+
 
 def op2_3_bcnf_4_5(relations):
     for relation_name, relation in relations.items():
-        primary_keys = relation_name if isinstance(relation_name, tuple) else (relation_name,)
-        table_name = "_".join(primary_keys) + "_table"
+        Primarykeys = relation_name
+        Primarykeys = (Primarykeys,) if isinstance(
+            Primarykeys, str) else Primarykeys
+        table_name = "_".join(Primarykeys) + '_table'
 
-        query = f"CREATE TABLE {table_name} (\n"
-        
-        for column, dtype in zip(relation.columns, relation.dtypes):
-            if column in primary_keys:
-                query += f"  {column} {querydatatypes(dtype)} PRIMARY KEY,\n"
-            else:
-                query += f"  {column} {querydatatypes(dtype)},\n"
-        
-        # Remove the last comma and add closing parenthesis
-        query = query.rstrip(',\n') + "\n);"
+        if table_name.count('_') >= 2:
+            query = f"CREATE TABLE {table_name} (\n"
+
+            for column, dtype in zip(relation.columns, relation.dtypes):
+                if column in Primarykeys:
+                    query += f" FOREIGN KEY ({column}) REFERENCES {column.replace('_fk','')}_table({column.replace('_fk','')}),\n"
+                else:
+                    query += f"  {column} {querydatatypes(dtype)},\n"
+
+            query = query.rstrip(',\n') + "\n);"
+        else:
+            query = f"CREATE TABLE {table_name} (\n"
+
+            for column, dtype in zip(relation.columns, relation.dtypes):
+                if column in Primarykeys:
+                    query += f"  {column} {querydatatypes(dtype)} PRIMARY KEY,\n"
+                else:
+                    query += f"  {column} {querydatatypes(dtype)},\n"
+
+            query = query.rstrip(',\n') + "\n);"
+
         print(query)
 
